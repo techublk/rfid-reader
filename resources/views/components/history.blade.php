c301ec30466bb7ca353a935f885a1c5d809ccde1
thilanRandika <dgtrandika@gmail.com>
2023-12-29 18:36:23 +0530

    arrange components of home page

---
+++ resources/views/components/history.blade.php
@@ -0,0 +1,46 @@
+<div class="history">
+
+    <table class="table table-striped table-success table-striped mt-5 text-center">
+        <thead>
+            <tr>
+                <th scope="col">C0</th>
+                <th scope="col">c1</th>
+                <th scope="col">c2</th>
+                <th scope="col">c3</th>
+                <th scope="col">timeStamp</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($colorArr as $item)
+                
+            
+                    
+            <tr>
+                        <td id='data_c0'>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="36" height="36" fill="currentColor" class="bi bi-circle-fill status-icon {{ $item->c0 == 1 ? 'green' : ($item->c0 == 0 ? 'red' : '') }}" viewBox="0 0 16 16">
+                                <circle cx="8" cy="8" r="8"/>
+                            </svg>
+                        </td>
+                        <td id='data_c1'>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="36" height="36" fill="currentColor" class="bi bi-circle-fill status-icon {{ $item->c1== 1 ? 'green' : ($item->c1 == 0 ? 'red' : '') }}" viewBox="0 0 16 16">
+                                <circle cx="8" cy="8" r="8"/>
+                            </svg>
+                        </td>
+                        <td id='data_c2'>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="36" height="36" fill="currentColor" class="bi bi-circle-fill status-icon {{ $item->c2 == 1 ? 'green' : ($item->c2 == 0 ? 'red' : '') }}" viewBox="0 0 16 16">
+                                <circle cx="8" cy="8" r="8"/>
+                            </svg>
+                        </td>
+                        <td id='data_c3'>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="36" height="36" fill="currentColor" class="bi bi-circle-fill status-icon {{$item->c3 == 1 ? 'green' : ($item->c3 == 0 ? 'red' : '') }}" viewBox="0 0 16 16">
+                                <circle cx="8" cy="8" r="8"/>
+                            </svg>
+                        </td>
+                        <td id='data_time'>
+                            {{ $item->created_at }}
+                        </td>
+                    </tr>
+                    @endforeach
+                </tbody>
+            </table>
+    </div>
