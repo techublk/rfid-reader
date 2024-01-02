5810a62ffe5885ed8cf11db8e03373594d4b60ca
thilanRandika <dgtrandika@gmail.com>
2024-01-03 03:06:23 +0530

    add styles to the login page

--- resources/views/pages/auth/login.blade.php
+++ resources/views/pages/auth/login.blade.php
@@ -1,11 +1,13 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container">
+    <div class="container-fluid bg-dark text-light" style="height: 100vh;">
         <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">{{ __('Login') }}</div>
+            <div class="col-md-6 my-5">
+                <div class="card mx-auto border-light" style="background-color: #1e1e1e; border-color: #343a40; margin-top: 40px;">
+                    <div class="card-header" style="background-color: #343a40; border-bottom: none;">
+                        <h2 class="text-light mb-0">{{ __('Login') }}</h2>
+                    </div>
 
                     <div class="card-body">
                         @if ($errors->any())
@@ -17,28 +19,20 @@
                         <form method="POST" action="{{ route('login') }}">
                             @csrf
 
-                            <div class="form-group row">
-                                <label for="username" class="col-md-4 col-form-label text-md-right">{{ __('Username') }}</label>
-
-                                <div class="col-md-6">
-                                    <input id="username" type="text" class="form-control" name="username" required autofocus>
-                                </div>
+                            <div class="mb-3">
+                                <label for="username" class="form-label text-light">{{ __('Username') }}</label>
+                                <input id="username" type="text" class="form-control" name="username" required autofocus>
                             </div>
 
-                            <div class="form-group row">
-                                <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                                <div class="col-md-6">
-                                    <input id="password" type="password" class="form-control" name="password" required>
-                                </div>
+                            <div class="mb-3">
+                                <label for="password" class="form-label text-light">{{ __('Password') }}</label>
+                                <input id="password" type="password" class="form-control" name="password" required>
                             </div>
 
-                            <div class="form-group row mb-0">
-                                <div class="col-md-6 offset-md-4">
-                                    <button type="submit" class="btn btn-primary">
-                                        {{ __('Login') }}
-                                    </button>
-                                </div>
+                            <div class="mb-3">
+                                <button type="submit" class="btn btn-light" style="background-color: #6c757d; border-color: #6c757d;">
+                                    {{ __('Login') }}
+                                </button>
                             </div>
                         </form>
                     </div>
